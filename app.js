ec168b4210ed54d6a52135a38b1836e69e8c0777
Nishant Kumar <me.nishantkumar99@gmail.com>
2018-02-12 22:08:32 -0800

    2nd commit

--- app.js
+++ app.js
@@ -1,2 +1,13 @@
 var myapp = angular.module('myapp',[]);
-console.log(myapp);
+
+//Creating a new controller under MyApp module.
+myapp.controller('MyCtrl',['$scope',function($scope){    
+    $scope.myval = "Hello World.";
+    //ng-model helps in establishing 2-way binding.
+    
+    //Button On Click Event.    
+    $scope.OnClick = function(){
+        //alert("Clicked");
+        $scope.myval = "Some New Value";
+    };
+}]);
